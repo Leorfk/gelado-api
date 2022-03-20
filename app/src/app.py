9b1c8c54b028bb5e692d86d3cc80eae4ab058f2f
leonardo rodrigues <leonardorodrigues_f@hotmail.com>
2022-03-20 19:17:47 -0300

    teste usando o fastapi

--- app/src/app.py
+++ app/src/app.py
@@ -1,75 +1,12 @@
-from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+import uvicorn
+from fastapi import FastAPI
+from controllers import app_controller
 
+def create_app():
+    fast_app = FastAPI()
+    fast_app.include_router(app_controller.router)
+    return fast_app
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-
-
-@app.route("/")
-def tela_inicial():
-    return render_template('home.html')
-
-
-@app.route("/", methods=["POST"])
-def main_page():
-    if request.method == "POST":
-        title = request.form["title"]
-        description = request.form["description"]
-        todo = Todo(title=title, description=description)
-        db.session.add(todo)
-        db.session.commit()
-        alltodos = Todo.query.all()
-    return render_template("home.html", todos=alltodos)
-
-
-@app.route('/get-all-todo', methods=['GET'])
-def get_all():
-    todos = Todo.query.all()
-    return render_template("home.html", todos=todos)
-
-
-@app.route("/delete/<int:sno>")
-def delete(sno):
-    deletetodo = Todo.query.filter_by(sno=sno).first()
-    db.session.delete(deletetodo)
-    db.session.commit()
-    return redirect('/')
-
-
-@app.route('/update/<int:sno>', methods=['GET'])
-def vai_kct(sno):
-    updatetodo = Todo.query.filter_by(sno=sno).first()
-    return render_template("update.html",
-                           updatetodo=updatetodo)
-
-
-@app.route("/updatetodo/<int:sno>", methods=["POST"])
-def update_todo(sno):
-    if request.method == "POST":
-        updatetodo = Todo.query.filter_by(sno=sno).first()
-        title = request.form["title"]
-        description = request.form["description"]
-        todo = Todo(title=title, description=description)
-        updatetodo.title = title
-        updatetodo.description = description
-        db.session.commit()
-        return redirect("/")
-    updatetodo = Todo.query.filter_by(sno=sno).first()
-    return render_template("update.html",
-                           updatetodo=updatetodo)
-
-
-class Todo(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    description = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return "{} is the title and {} is the description".format(self.title, self.description)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app = create_app()
+    uvicorn.run('app:create_app', reload=True)
--- app/src/main.py
+++ app/src/main.py
@@ -3,14 +3,4 @@ from models.user_role_model import User_Role
 from repositories.user_role_repository import UserRoleRepository
 import peewee
 if __name__ == '__main__':
-    role = UserRole(70, 'XAMBRE')
-    repo = UserRoleRepository(User_Role)
-    # repo.create_user_role(role)
-    # role = repo.get_user_role_by_id(9)
-    # print(xap.id_role)
-    # print(xap.texto_role)
-    # roles = repo.get_all_user_roles()
-    # for role in roles:
-    #     print(role.id_role)
-    # repo.update_user_role(7, role)
-    # repo.delete_user_role(7)
+    pass
